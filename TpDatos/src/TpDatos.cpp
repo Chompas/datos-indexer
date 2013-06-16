31c56dde6a66227f889024182033f9e1edd33ce6
javierlara <javi29c@hotmail.com>
2013-06-16 17:40:10 -0300

    HASTA ACA ANDA PERO EL MERGE ES LENTO

--- TpDatos/src/TpDatos.cpp
+++ TpDatos/src/TpDatos.cpp
@@ -220,7 +220,7 @@ int main(int argc, char** argv) {
 		}*/
 	//DESCOMENTAR ESTO Y COMENTAR EL OTRO
 	//	indexar(argv[2], argv[3]);
-	indexar("probando","prueba");
+//	indexar("probando","books");
 
 /*
 	} else if (instruccion == "q") {
@@ -247,7 +247,7 @@ int main(int argc, char** argv) {
 */
 		//DESCOMENTAR ESTO Y COMENTAR EL OTRO
 		//consulta(r,q);
-	//	consulta("probando"," ebook");
+		consulta("probando"," andromache");
 //
 //	} else {
 //		cout << "Instrucción inválida" << endl;
